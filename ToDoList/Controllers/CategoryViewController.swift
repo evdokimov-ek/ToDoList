8b149260a19aca5eba43fc48a48aca357c7352f8
Evgeniy Evdokimov <evdokimov.ek@gmail.com>
2021-09-29 22:30:42 +0300

    add CategoryViewController

---
+++ ToDoList/Controllers/CategoryViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryViewController.swift
+//  ToDoList
+//
+//  Created by 19336088 on 29.09.2021.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    //MARK: - TableView DataSource Methods
+    
+    
+    //MARK: - TableView Delegates Methods
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+    
+}
